1c98db1bc97a5887b4f5eba9ab604e4acb074de7
Marco Peereboom <marco@peereboom.us>
2018-07-10 12:36:26 +0100

    Add dump/restore to backend interface.
    We need to be able to dump/restore the backend database. Add this
    functionality to the backend interface to make it mandatory.
    
    This functionality also enables future migration from one backend to
    another.

--- cmd/dcrtime_dumpdb/dcrtime_dumpdb.go
+++ cmd/dcrtime_dumpdb/dcrtime_dumpdb.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/decred/dcrd/dcrutil"
 	"github.com/decred/dcrtime/dcrtimed/backend/filesystem"
@@ -11,13 +12,48 @@ import (
 
 var (
 	defaultHomeDir = dcrutil.AppDataDir("dcrtimed", false)
-	fsRoot         = flag.String("d", defaultHomeDir, "Backend directory")
+	testnet        = flag.Bool("testnet", false, "Use testnet port")
+	dumpJSON       = flag.Bool("json", false, "Dump JSON")
+	restore        = flag.Bool("restore", false, "Restore backend, -destination is required")
+	destination    = flag.String("destination", "", "Restore destination")
+	fsRoot         = flag.String("source", defaultHomeDir, "Source directory")
 )
 
 func _main() error {
 	flag.Parse()
 
-	return filesystem.DumpAll(*fsRoot)
+	if *restore {
+		if *destination == "" {
+			return fmt.Errorf("-destination must be set")
+		}
+	}
+
+	if *restore {
+		// Restore
+		fs, err := filesystem.NewRestore(*destination)
+		if err != nil {
+			return err
+		}
+		defer fs.Close()
+
+		return fs.Restore(os.Stdin, true, *destination)
+	}
+
+	// Dump
+	var root string
+	if *testnet {
+		root = filepath.Join(*fsRoot, "data", "testnet2")
+	} else {
+		root = filepath.Join(*fsRoot, "data", "mainnet")
+	}
+
+	fs, err := filesystem.NewDump(root)
+	if err != nil {
+		return err
+	}
+	defer fs.Close()
+
+	return fs.Dump(os.Stdout, !*dumpJSON)
 }
 
 func main() {
