ba5803a5ae70b6922c0e0f7b54c78cfcb8a200b4
David Hill <dhill@mindcry.org>
2019-10-18 15:17:54 -0400

    go.mod: update deps

--- dcrtimed/params.go
+++ dcrtimed/params.go
@@ -6,7 +6,7 @@
 package main
 
 import (
-	"github.com/decred/dcrd/chaincfg"
+	"github.com/decred/dcrd/chaincfg/v2"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrwallet/netparams"
 )
@@ -29,7 +29,7 @@ type params struct {
 // it does not handle on to dcrd.  This approach allows the wallet process
 // to emulate the full reference implementation RPC API.
 var mainNetParams = params{
-	Params:              &chaincfg.MainNetParams,
+	Params:              chaincfg.MainNetParams(),
 	WalletRPCServerPort: netparams.MainNetParams.GRPCServerPort,
 }
 
@@ -38,14 +38,14 @@ var mainNetParams = params{
 // reference implementation - see the mainNetParams comment for details.
 
 var testNet3Params = params{
-	Params:              &chaincfg.TestNet3Params,
+	Params:              chaincfg.TestNet3Params(),
 	WalletRPCServerPort: netparams.TestNet3Params.GRPCServerPort,
 }
 
 // simNetParams contains parameters specific to the simulation test network
 // (wire.SimNet).
 var simNetParams = params{
-	Params:              &chaincfg.SimNetParams,
+	Params:              chaincfg.SimNetParams(),
 	WalletRPCServerPort: netparams.SimNetParams.GRPCServerPort,
 }
 
