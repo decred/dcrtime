2cf8fd283ba6ffe85a9f7c4a26b7c46840e38333
Thiago Figueiredo <thiagofigueiredo777@gmail.com>
2020-03-01 11:51:24 -0300

    Add cli flag to choose API version for commands

--- cmd/dcrtime/dcrtime.go
+++ cmd/dcrtime/dcrtime.go
@@ -19,6 +19,7 @@ import (
 	"os"
 	"strconv"
 
+	v1 "github.com/decred/dcrtime/api/v1"
 	v2 "github.com/decred/dcrtime/api/v2"
 	"github.com/decred/dcrtime/merkle"
 	"github.com/decred/dcrtime/util"
@@ -42,6 +43,8 @@ var (
 		` for accessing privileged API resources"`)
 	balance = flag.Bool("balance", false, `long:"balance" description:"Display`+
 		` the connected server's wallet balance. An API Token is required"`)
+	apiVersion = flag.Int("api", 0,
+		"Inform the API version to be used by the cli (1 or 2)")
 )
 
 // normalizeAddress returns addr with the passed default port appended if
@@ -138,7 +141,178 @@ func newClient(skipVerify bool) *http.Client {
 	return &http.Client{Transport: tr}
 }
 
-func download(questions []string) error {
+func downloadV1(questions []string) error {
+	ver := v1.Verify{
+		ID: dcrtimeClientID,
+	}
+
+	for _, question := range questions {
+		if ts, ok := convertTimestamp(question); ok {
+			ver.Timestamps = append(ver.Timestamps, ts)
+			continue
+		}
+
+		if isDigest(question) {
+			ver.Digests = append(ver.Digests, question)
+			continue
+		}
+
+		return fmt.Errorf("not a digest or timestamp: %v", question)
+	}
+
+	// Convert Verify to JSON
+	b, err := json.Marshal(ver)
+	if err != nil {
+		return err
+	}
+
+	if *debug {
+		fmt.Println(string(b))
+	}
+
+	// If this is a trial run return.
+	if *trial {
+		return nil
+	}
+
+	c := newClient(false)
+	r, err := c.Post(*host+v1.VerifyRoute, "application/json",
+		bytes.NewReader(b))
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != http.StatusOK {
+		e, err := getError(r.Body)
+		if err != nil {
+			return fmt.Errorf("%v", r.Status)
+		}
+		return fmt.Errorf("%v: %v", r.Status, e)
+	}
+
+	if *printJson {
+		io.Copy(os.Stdout, r.Body)
+		fmt.Printf("\n")
+		return nil
+	}
+
+	// Decode response.
+	var vr v1.VerifyReply
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&vr); err != nil {
+		return fmt.Errorf("could node decode VerifyReply: %v", err)
+	}
+
+	for _, v := range vr.Timestamps {
+		result, ok := v1.Result[v.Result]
+		if !ok {
+			fmt.Printf("%v invalid error code %v\n", v.ServerTimestamp,
+				v.Result)
+			continue
+		}
+
+		// Verify results if the collection is anchored.
+		if v.CollectionInformation.ChainTimestamp != 0 {
+			// Calculate merkle root of all digests.
+			digests := make([]*[sha256.Size]byte, 0,
+				len(v.CollectionInformation.Digests))
+			for _, digest := range v.CollectionInformation.Digests {
+				d, ok := convertDigest(digest)
+				if !ok {
+					return fmt.Errorf("Invalid digest "+
+						"server response for "+
+						"timestamp: %v",
+						v.ServerTimestamp)
+				}
+				digests = append(digests, &d)
+			}
+			root := merkle.Root(digests)
+			if hex.EncodeToString(root[:]) !=
+				v.CollectionInformation.MerkleRoot {
+				fmt.Printf("invalid merkle root: %v\n", err)
+			}
+		}
+
+		// Print the good news.
+		if v.CollectionInformation.ChainTimestamp == 0 &&
+			v.Result == v1.ResultOK {
+			result = "Not anchored"
+		}
+		fmt.Printf("%v %v\n", v.ServerTimestamp, result)
+
+		if !*verbose {
+			continue
+		}
+
+		prefix := "Digests"
+		for _, digest := range v.CollectionInformation.Digests {
+			fmt.Printf("  %-15v: %v\n", prefix, digest)
+			prefix = ""
+		}
+
+		// Only print additional info if we are anchored
+		if v.CollectionInformation.ChainTimestamp == 0 {
+			continue
+		}
+		fmt.Printf("  %-15v: %v\n", "Chain Timestamp",
+			v.CollectionInformation.ChainTimestamp)
+		fmt.Printf("  %-15v: %v\n", "Merkle Root",
+			v.CollectionInformation.MerkleRoot)
+		fmt.Printf("  %-15v: %v\n", "TxID",
+			v.CollectionInformation.Transaction)
+	}
+
+	for _, v := range vr.Digests {
+		result, ok := v1.Result[v.Result]
+		if !ok {
+			fmt.Printf("%v invalid error code %v\n", v.Digest,
+				v.Result)
+			continue
+		}
+
+		// Verify merkle path.
+		root, err := merkle.VerifyAuthPath(&v.ChainInformation.MerklePath)
+		if err != nil {
+			if err != merkle.ErrEmpty {
+				fmt.Printf("%v invalid auth path %v\n",
+					v.Digest, err)
+				continue
+			}
+			fmt.Printf("%v Not anchored\n", v.Digest)
+			continue
+		}
+
+		// Verify merkle root.
+		merkleRoot, err := hex.DecodeString(v.ChainInformation.MerkleRoot)
+		if err != nil {
+			fmt.Printf("invalid merkle root: %v\n", err)
+			continue
+		}
+		// This is silly since we check against returned root.
+		if !bytes.Equal(root[:], merkleRoot) {
+			fmt.Printf("%v invalid merkle root\n", v.Digest)
+			continue
+		}
+
+		// Print the good news.
+		fmt.Printf("%v %v\n", v.Digest, result)
+
+		if !*verbose {
+			continue
+		}
+		fmt.Printf("  %-15v: %v\n", "Chain Timestamp",
+			v.ChainInformation.ChainTimestamp)
+		fmt.Printf("  %-15v: %v\n", "Merkle Root",
+			v.ChainInformation.MerkleRoot)
+		fmt.Printf("  %-15v: %v\n", "TxID",
+			v.ChainInformation.Transaction)
+	}
+
+	return nil
+}
+
+func downloadV2(questions []string) error {
 	ver := v2.VerifyBatch{
 		ID: dcrtimeClientID,
 	}
@@ -309,7 +483,74 @@ func download(questions []string) error {
 	return nil
 }
 
-func upload(digests []string, exists map[string]string) error {
+func uploadV1(digests []string, exists map[string]string) error {
+	// batch uploads
+	ts := v1.Timestamp{
+		ID:      dcrtimeClientID,
+		Digests: digests,
+	}
+	b, err := json.Marshal(ts)
+	if err != nil {
+		return err
+	}
+
+	if *debug {
+		fmt.Println(string(b))
+	}
+
+	// If this is a trial run return.
+	if *trial {
+		return nil
+	}
+
+	c := newClient(false)
+	r, err := c.Post(*host+v1.TimestampRoute, "application/json",
+		bytes.NewReader(b))
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != http.StatusOK {
+		e, err := getError(r.Body)
+		if err != nil {
+			return fmt.Errorf("%v", r.Status)
+		}
+		return fmt.Errorf("%v: %v", r.Status, e)
+	}
+
+	if *printJson {
+		io.Copy(os.Stdout, r.Body)
+		fmt.Printf("\n")
+		return nil
+	}
+
+	// Decode response.
+	var tsReply v1.TimestampReply
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&tsReply); err != nil {
+		return fmt.Errorf("Could node decode TimestampReply: %v", err)
+	}
+
+	// Print human readable results.
+	for k, v := range tsReply.Results {
+		filename := exists[tsReply.Digests[k]]
+		if v == v1.ResultOK {
+			fmt.Printf("%v OK %v\n", tsReply.Digests[k], filename)
+			continue
+		}
+		fmt.Printf("%v Exists %v\n", tsReply.Digests[k], filename)
+	}
+
+	if *verbose {
+		// Print server timestamp.
+		fmt.Printf("Collection timestamp: %v\n", tsReply.ServerTimestamp)
+	}
+
+	return nil
+}
+
+func uploadV2(digests []string, exists map[string]string) error {
 	// batch uploads
 	ts := v2.TimestampBatch{
 		ID:      dcrtimeClientID,
@@ -362,7 +603,7 @@ func upload(digests []string, exists map[string]string) error {
 	for k, v := range tsReply.Results {
 		filename := exists[tsReply.Digests[k]]
 		if v == v2.ResultOK {
-			fmt.Printf("%v OK     %v\n", tsReply.Digests[k], filename)
+			fmt.Printf("%v OK %v\n", tsReply.Digests[k], filename)
 			continue
 		}
 		fmt.Printf("%v Exists %v\n", tsReply.Digests[k], filename)
@@ -380,8 +621,8 @@ func hasDigestFlag() bool {
 	return digest != nil && *digest != ""
 }
 
-func uploadDigest(digest string) error {
-	return upload([]string{digest}, make(map[string]string))
+func hasAPIVersionFlag() bool {
+	return apiVersion != nil && *apiVersion != 0
 }
 
 // Ensures that there are no conflicting flags
@@ -495,17 +736,42 @@ func _main() error {
 		return flagError
 	}
 
+	var mainnetHost string
+	var testnetHost string
+	var mainnetPort string
+	var testnetPort string
+	var upload func([]string, map[string]string) error
+	var download func([]string) error
+
+	// Check if API version flag is set to v1. If not, default
+	// to v2.
+	if hasAPIVersionFlag() && *apiVersion == v1.APIVersion {
+		mainnetHost = v1.DefaultMainnetTimeHost
+		testnetHost = v1.DefaultTestnetTimeHost
+		mainnetPort = v1.DefaultMainnetTimePort
+		testnetPort = v1.DefaultTestnetTimePort
+		upload = uploadV1
+		download = downloadV1
+	} else {
+		mainnetHost = v2.DefaultMainnetTimeHost
+		testnetHost = v2.DefaultTestnetTimeHost
+		mainnetPort = v2.DefaultMainnetTimePort
+		testnetPort = v2.DefaultTestnetTimePort
+		upload = uploadV2
+		download = downloadV2
+	}
+
 	if *host == "" {
 		if *testnet {
-			*host = v2.DefaultTestnetTimeHost
+			*host = testnetHost
 		} else {
-			*host = v2.DefaultMainnetTimeHost
+			*host = mainnetHost
 		}
 	}
 
-	port := v2.DefaultMainnetTimePort
+	port := mainnetPort
 	if *testnet {
-		port = v2.DefaultTestnetTimePort
+		port = testnetPort
 	}
 
 	*host = normalizeAddress(*host, port)
@@ -520,7 +786,7 @@ func _main() error {
 	// Allow submitting a pre-calculated 256 bit digest from the command line,
 	// rather than needing to hash a payload.
 	if hasDigestFlag() {
-		return uploadDigest(*digest)
+		return upload([]string{*digest}, make(map[string]string))
 	}
 
 	// Print the wallet balance via privileged endpoint.
@@ -536,8 +802,8 @@ func _main() error {
 	// We attempt to open files first; if that doesn't work we treat the
 	// args as digests or timestamps.  Digests and timestamps are sent to
 	// the server for lookup.  Use fileOnly to override this behavior.
-	var uploads []string
-	var downloads []string
+	var uploadArr []string
+	var downloadArr []string
 	exists := make(map[string]string) // [digest]filename
 	for _, a := range flag.Args() {
 		// Try to see if argument is a valid file.
@@ -555,7 +821,7 @@ func _main() error {
 			}
 			exists[d] = a
 
-			uploads = append(uploads, d)
+			uploadArr = append(uploadArr, d)
 			if *verbose {
 				fmt.Printf("%v Upload %v\n", d, a)
 			}
@@ -565,7 +831,7 @@ func _main() error {
 		// Argument was not a file, try to see if it is a digest or
 		// timestamp instead.
 		if isDigest(a) || isTimestamp(a) {
-			downloads = append(downloads, a)
+			downloadArr = append(downloadArr, a)
 			if *verbose {
 				fmt.Printf("%-64v Verify\n", a)
 			}
@@ -580,19 +846,19 @@ func _main() error {
 			a)
 	}
 
-	if len(uploads) == 0 && len(downloads) == 0 && !didRunCommand {
+	if len(uploadArr) == 0 && len(downloadArr) == 0 && !didRunCommand {
 		return fmt.Errorf("nothing to do")
 	}
 
-	if len(uploads) != 0 {
-		err := upload(uploads, exists)
+	if len(uploadArr) != 0 {
+		err := upload(uploadArr, exists)
 		if err != nil {
 			return err
 		}
 	}
 
-	if len(downloads) != 0 {
-		err := download(downloads)
+	if len(downloadArr) != 0 {
+		err := download(downloadArr)
 		if err != nil {
 			return err
 		}
--- cmd/dcrtime_checker/dcrtime_checker.go
+++ cmd/dcrtime_checker/dcrtime_checker.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"strings"
 
+	v1 "github.com/decred/dcrtime/api/v1"
 	v2 "github.com/decred/dcrtime/api/v2"
 	"github.com/decred/dcrtime/merkle"
 	"github.com/decred/dcrtime/util"
@@ -20,64 +21,22 @@ var (
 	dcrdataHost = flag.String("h", "", "dcrdata host")
 	testnet     = flag.Bool("testnet", false, "Use testnet port")
 	verbose     = flag.Bool("v", false, "Verbose")
+	apiVersion  = flag.Int("api", 0,
+		"Inform the API version to be used by the cli (1 or 2)")
 )
 
-func _main() error {
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "dcrtime_checker [-h {dcrdatahost}|"+
-			"-testnet|-v] -f {file} -p {proof}\n\n")
-		flag.PrintDefaults()
-	}
-	flag.Parse()
-
-	// require -f
-	if *file == "" {
-		return fmt.Errorf("must provide -f")
-	}
-
-	// require -p
-	if *proof == "" {
-		return fmt.Errorf("must provide -p")
-	}
-
-	// Ensure proof looks correct
-	fProof, err := os.Open(*proof)
-	if err != nil {
-		return err
-	}
+func verifyV2(digest string, fProof *os.File) error {
 	var vr v2.VerifyBatchReply
 	decoder := json.NewDecoder(fProof)
 	if err := decoder.Decode(&vr); err != nil {
-		return fmt.Errorf("Could node decode VerifyReply: %v", err)
-	}
-
-	// Handle dcrtime host
-	if *dcrdataHost == "" {
-		if *testnet {
-			*dcrdataHost = "https://testnet.dcrdata.org/api/tx/"
-		} else {
-			*dcrdataHost = "https://explorer.dcrdata.org/api/tx/"
-		}
-	} else {
-		if !strings.HasSuffix(*dcrdataHost, "/") {
-			*dcrdataHost += "/"
-		}
-	}
-	// Get file digest
-	d, err := util.DigestFile(*file)
-	if err != nil {
-		return err
-	}
-
-	if *verbose {
-		fmt.Printf("%v  %v\n", d, *file)
+		return fmt.Errorf("Could node decode VerifyBatchReply: %v", err)
 	}
 
 	// Ensure file digest exists in the proof and that the saved answer was
 	// correct
 	found := -1
 	for k, v := range vr.Digests {
-		if v.Digest != d {
+		if v.Digest != digest {
 			continue
 		}
 
@@ -102,13 +61,13 @@ func _main() error {
 			return fmt.Errorf("%v invalid auth path %v",
 				v.Digest, err)
 		}
-		return fmt.Errorf("%v Not anchored\n", v.Digest)
+		return fmt.Errorf("%v Not anchored", v.Digest)
 	}
 
 	// Verify merkle root.
 	merkleRoot, err := hex.DecodeString(v.ChainInformation.MerkleRoot)
 	if err != nil {
-		return fmt.Errorf("invalid merkle root: %v\n", err)
+		return fmt.Errorf("invalid merkle root: %v", err)
 	}
 	// This is silly since we check against returned root.
 	if !bytes.Equal(root[:], merkleRoot) {
@@ -117,7 +76,7 @@ func _main() error {
 
 	// If we made it here we have a valid proof
 	if *verbose {
-		fmt.Printf("%v  Proof  OK\n", d)
+		fmt.Printf("%v  Proof  OK\n", digest)
 	}
 
 	// Verify against dcrdata
@@ -128,7 +87,144 @@ func _main() error {
 	}
 
 	if *verbose {
-		fmt.Printf("%v  Anchor OK\n", d)
+		fmt.Printf("%v  Anchor OK\n", digest)
+	}
+
+	return nil
+}
+
+func verifyV1(digest string, fProof *os.File) error {
+	var vr v1.VerifyReply
+	decoder := json.NewDecoder(fProof)
+	if err := decoder.Decode(&vr); err != nil {
+		return fmt.Errorf("Could node decode VerifyReply: %v", err)
+	}
+
+	// Ensure file digest exists in the proof and that the saved answer was
+	// correct
+	found := -1
+	for k, v := range vr.Digests {
+		if v.Digest != digest {
+			continue
+		}
+
+		found = k
+		break
+	}
+	if found == -1 {
+		return fmt.Errorf("file digest not found in proof")
+	}
+	v := vr.Digests[found]
+
+	// Verify result of matching digest
+	if _, ok := v1.Result[v.Result]; !ok {
+		return fmt.Errorf("%v invalid error code %v", v.Digest,
+			v.Result)
+	}
+
+	// Verify merkle path.
+	root, err := merkle.VerifyAuthPath(&v.ChainInformation.MerklePath)
+	if err != nil {
+		if err != merkle.ErrEmpty {
+			return fmt.Errorf("%v invalid auth path %v",
+				v.Digest, err)
+		}
+		return fmt.Errorf("%v Not anchored", v.Digest)
+	}
+
+	// Verify merkle root.
+	merkleRoot, err := hex.DecodeString(v.ChainInformation.MerkleRoot)
+	if err != nil {
+		return fmt.Errorf("invalid merkle root: %v", err)
+	}
+	// This is silly since we check against returned root.
+	if !bytes.Equal(root[:], merkleRoot) {
+		return fmt.Errorf("%v invalid merkle root", v.Digest)
+	}
+
+	// If we made it here we have a valid proof
+	if *verbose {
+		fmt.Printf("%v  Proof  OK\n", digest)
+	}
+
+	// Verify against dcrdata
+	err = util.VerifyAnchor(*dcrdataHost,
+		vr.Digests[found].ChainInformation.Transaction, root[:])
+	if err != nil {
+		return err
+	}
+
+	if *verbose {
+		fmt.Printf("%v  Anchor OK\n", digest)
+	}
+
+	return nil
+}
+
+func hasAPIVersionFlag() bool {
+	return apiVersion != nil && *apiVersion != 0
+}
+
+func _main() error {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "dcrtime_checker [-h {dcrdatahost}|"+
+			"-testnet|-v] -f {file} -p {proof}\n\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	var verify func(string, *os.File) error
+
+	// Check if API version flag is set to v1. If not, default
+	// to v2.
+	if hasAPIVersionFlag() && *apiVersion == v1.APIVersion {
+		verify = verifyV1
+	} else {
+		verify = verifyV2
+	}
+
+	// require -f
+	if *file == "" {
+		return fmt.Errorf("must provide -f")
+	}
+
+	// require -p
+	if *proof == "" {
+		return fmt.Errorf("must provide -p")
+	}
+
+	// Handle dcrtime host
+	if *dcrdataHost == "" {
+		if *testnet {
+			*dcrdataHost = "https://testnet.dcrdata.org/api/tx/"
+		} else {
+			*dcrdataHost = "https://explorer.dcrdata.org/api/tx/"
+		}
+	} else {
+		if !strings.HasSuffix(*dcrdataHost, "/") {
+			*dcrdataHost += "/"
+		}
+	}
+
+	// Ensure proof looks correct
+	fProof, err := os.Open(*proof)
+	if err != nil {
+		return err
+	}
+
+	// Get file digest
+	d, err := util.DigestFile(*file)
+	if err != nil {
+		return err
+	}
+
+	if *verbose {
+		fmt.Printf("%v  %v\n", d, *file)
+	}
+
+	err = verify(d, fProof)
+	if err != nil {
+		return err
 	}
 
 	return nil
