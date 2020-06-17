dbbf4c2adcd808b74fde912999413338831eeb72
amass <amassarwi@gmail.com>
2020-06-17 19:44:52 +0200

    CI: fail if go.mod/go.sum include changes

--- goclean.sh
+++ goclean.sh
@@ -13,6 +13,17 @@ set -ex
 # run tests
 env GORACE="halt_on_error=1" go test -race ./...
 
+# run `go mod download` and `go mod tidy` and fail if the git status of
+# go.mod and/or go.sum changes
+MOD_STATUS=$(git status --porcelain go.mod go.sum)
+go mod download
+go mod tidy
+UPDATED_MOD_STATUS=$(git status --porcelain go.mod go.sum)
+if [ "$UPDATED_MOD_STATUS" != "$MOD_STATUS" ]; then
+  echo "Running `go mod tidy` modified go.mod and/or go.sum"
+  exit 1
+fi
+
 # golangci-lint (github.com/golangci/golangci-lint) is used to run each each
 # static checker.
 
