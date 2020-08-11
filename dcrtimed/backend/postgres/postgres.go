68add0f0076b858ed4067027297a55207df11b40
amass <amassarwi@gmail.com>
2020-08-11 20:34:06 +0300

    move sql logic to separate file - postgres/sql.go

--- dcrtimed/backend/postgres/postgres.go
+++ dcrtimed/backend/postgres/postgres.go
@@ -83,23 +83,6 @@ func (pg *Postgres) GetTimestamps([]int64) ([]backend.TimestampResult, error) {
 	return nil, nil
 }
 
-func (pg *Postgres) checkIfDigestExists(hash []byte) (bool, error) {
-	rows, err := pg.db.Query(`SELECT EXISTS (SELECT FROM records 
-		WHERE digest = $1)`, hash)
-	if err != nil {
-		return false, err
-	}
-	defer rows.Close()
-	var exists bool
-	for rows.Next() {
-		err = rows.Scan(&exists)
-		if err != nil {
-			return false, err
-		}
-	}
-	return exists, nil
-}
-
 // Store hashes and return timestamp and associated errors.  Put is
 // allowed to return transient errors.
 func (pg *Postgres) Put(hashes [][sha256.Size]byte) (int64, []backend.PutResult, error) {
@@ -229,128 +212,6 @@ func buildQueryString(rootCert, cert, key string) string {
 	return v.Encode()
 }
 
-func hasTable(db *sql.DB, name string) (bool, error) {
-	rows, err := db.Query(`SELECT EXISTS (SELECT FROM information_schema.tables 
-		WHERE table_schema = 'public' AND table_name  = $1)`, name)
-	if err != nil {
-		return false, err
-	}
-	defer rows.Close()
-	var exists bool
-	for rows.Next() {
-		err = rows.Scan(&exists)
-		if err != nil {
-			return false, err
-		}
-	}
-	return exists, nil
-}
-
-func createAnchorsTable(db *sql.DB) error {
-	_, err := db.Exec(`CREATE TABLE public.anchors
-(
-    merkle character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    hashes text[] COLLATE pg_catalog."default" NOT NULL,
-    tx_hash text COLLATE pg_catalog."default",
-    chain_timestamp bigint,
-    flush_timestamp bigint,
-    CONSTRAINT anchors_pkey PRIMARY KEY (merkle)
-);
--- Index: idx_chain_timestamp
-CREATE INDEX idx_chain_timestamp
-    ON public.anchors USING btree
-    (chain_timestamp ASC NULLS LAST)
-    TABLESPACE pg_default;
--- Index: idx_flush_timestamp
-CREATE INDEX idx_flush_timestamp
-    ON public.anchors USING btree
-    (flush_timestamp ASC NULLS LAST)
-    TABLESPACE pg_default;
--- Index: idx_hashes
-CREATE UNIQUE INDEX idx_hashes
-    ON public.anchors USING btree
-    (hashes COLLATE pg_catalog."default" ASC NULLS LAST)
-    TABLESPACE pg_default;
--- Index: idx_merkle
-CREATE UNIQUE INDEX idx_merkle
-    ON public.anchors USING btree
-    (merkle COLLATE pg_catalog."default" ASC NULLS LAST)
-    TABLESPACE pg_default;
--- Index: idx_tx_hash
-CREATE UNIQUE INDEX idx_tx_hash
-    ON public.anchors USING btree
-    (tx_hash COLLATE pg_catalog."default" ASC NULLS LAST)
-    TABLESPACE pg_default;
-`)
-	if err != nil {
-		return err
-	}
-	log.Infof("Anchors table created")
-	return nil
-}
-
-func createRecordsTable(db *sql.DB) error {
-	_, err := db.Exec(`CREATE TABLE public.records
-(
-    digest bytea NOT NULL,
-    anchor_merkle character varying(64) COLLATE pg_catalog."default",
-    key serial NOT NULL,
-    collection_timestamp text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT records_pkey PRIMARY KEY (key),
-    CONSTRAINT records_anchors_fkey FOREIGN KEY (anchor_merkle)
-        REFERENCES public.anchors (merkle) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-);
-
--- Index: fki_records_anchors_fkey
-CREATE INDEX fki_records_anchors_fkey
-    ON public.records USING btree
-    (anchor_merkle COLLATE pg_catalog."default" ASC NULLS LAST)
-    TABLESPACE pg_default;
--- Index: idx_collection_timestamp
-CREATE INDEX idx_collection_timestamp
-    ON public.records USING btree
-    (collection_timestamp COLLATE pg_catalog."default" ASC NULLS LAST)
-    TABLESPACE pg_default;
--- Index: idx_digest
-CREATE UNIQUE INDEX idx_digest
-    ON public.records USING btree
-    (digest ASC NULLS LAST)
-    TABLESPACE pg_default;
-`)
-	if err != nil {
-		return err
-	}
-	log.Infof("Records table created")
-	return nil
-}
-
-func createTables(db *sql.DB) error {
-	exists, err := hasTable(db, tableAnchors)
-	if err != nil {
-		return err
-	}
-	if !exists {
-		err = createAnchorsTable(db)
-		if err != nil {
-			return err
-		}
-	}
-	exists, err = hasTable(db, tableRecords)
-	if err != nil {
-		return err
-	}
-	if !exists {
-		err = createRecordsTable(db)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // internalNew creates the Pstgres context but does not launch background
 // bits.  This is used by the test packages.
 func internalNew(user, host, net, rootCert, cert, key string) (*Postgres, error) {
@@ -370,17 +231,17 @@ func internalNew(user, host, net, rootCert, cert, key string) (*Postgres, error)
 		return nil, fmt.Errorf("connect to database '%v': %v", addr, err)
 	}
 
-	// Create tables
-	err = createTables(db)
-	if err != nil {
-		return nil, err
-	}
-
 	pg := &Postgres{
 		cron:     cron.New(),
 		db:       db,
 		duration: duration,
 		myNow:    time.Now,
+	}
+
+	// Create tables
+	err = pg.createTables()
+	if err != nil {
+		return nil, err
 	}
 
 	return pg, nil
---
+++ dcrtimed/backend/postgres/sql.go
@@ -0,0 +1,140 @@
+package postgres
+
+func (pg *Postgres) hasTable(name string) (bool, error) {
+	rows, err := pg.db.Query(`SELECT EXISTS (SELECT FROM information_schema.tables 
+		WHERE table_schema = 'public' AND table_name  = $1)`, name)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+	var exists bool
+	for rows.Next() {
+		err = rows.Scan(&exists)
+		if err != nil {
+			return false, err
+		}
+	}
+	return exists, nil
+}
+
+func (pg *Postgres) checkIfDigestExists(hash []byte) (bool, error) {
+	rows, err := pg.db.Query(`SELECT EXISTS (SELECT FROM records 
+		WHERE digest = $1)`, hash)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+	var exists bool
+	for rows.Next() {
+		err = rows.Scan(&exists)
+		if err != nil {
+			return false, err
+		}
+	}
+	return exists, nil
+}
+
+func (pg *Postgres) createAnchorsTable() error {
+	_, err := pg.db.Exec(`CREATE TABLE public.anchors
+(
+    merkle character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    hashes text[] COLLATE pg_catalog."default" NOT NULL,
+    tx_hash text COLLATE pg_catalog."default",
+    chain_timestamp bigint,
+    flush_timestamp bigint,
+    CONSTRAINT anchors_pkey PRIMARY KEY (merkle)
+);
+-- Index: idx_chain_timestamp
+CREATE INDEX idx_chain_timestamp
+    ON public.anchors USING btree
+    (chain_timestamp ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: idx_flush_timestamp
+CREATE INDEX idx_flush_timestamp
+    ON public.anchors USING btree
+    (flush_timestamp ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: idx_hashes
+CREATE UNIQUE INDEX idx_hashes
+    ON public.anchors USING btree
+    (hashes COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: idx_merkle
+CREATE UNIQUE INDEX idx_merkle
+    ON public.anchors USING btree
+    (merkle COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: idx_tx_hash
+CREATE UNIQUE INDEX idx_tx_hash
+    ON public.anchors USING btree
+    (tx_hash COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+`)
+	if err != nil {
+		return err
+	}
+	log.Infof("Anchors table created")
+	return nil
+}
+
+func (pg *Postgres) createRecordsTable() error {
+	_, err := pg.db.Exec(`CREATE TABLE public.records
+(
+    digest bytea NOT NULL,
+    anchor_merkle character varying(64) COLLATE pg_catalog."default",
+    key serial NOT NULL,
+    collection_timestamp bigint NOT NULL,
+    CONSTRAINT records_pkey PRIMARY KEY (key),
+    CONSTRAINT records_anchors_fkey FOREIGN KEY (anchor_merkle)
+        REFERENCES public.anchors (merkle) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+-- Index: fki_records_anchors_fkey
+CREATE INDEX fki_records_anchors_fkey
+    ON public.records USING btree
+    (anchor_merkle COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: idx_collection_timestamp
+CREATE INDEX idx_collection_timestamp
+    ON public.records USING btree
+    (collection_timestamp ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: idx_digest
+CREATE UNIQUE INDEX idx_digest
+    ON public.records USING btree
+    (digest ASC NULLS LAST)
+    TABLESPACE pg_default;
+`)
+	if err != nil {
+		return err
+	}
+	log.Infof("Records table created")
+	return nil
+}
+
+func (pg *Postgres) createTables() error {
+	exists, err := pg.hasTable(tableAnchors)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		err = pg.createAnchorsTable()
+		if err != nil {
+			return err
+		}
+	}
+	exists, err = pg.hasTable(tableRecords)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		err = pg.createRecordsTable()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
